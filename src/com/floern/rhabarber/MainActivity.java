ba07e583230ef48feec5e260cd73035309ee4912
Florian Buenzli <dev@floern.com>
2012-11-12 12:55:23 +0100

    Basic Android Project created

---
+++ src/com/floern/rhabarber/MainActivity.java
@@ -0,0 +1,22 @@
+package com.floern.rhabarber;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // test
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
