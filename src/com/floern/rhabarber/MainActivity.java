2e314caab93b4844ed4f07af3af31f8b42f866c6
iliis <samuelbryner@gmx.ch>
2012-11-25 19:15:58 +0100

    removed physicstest from master, which I mistakenly added

--- src/com/floern/rhabarber/MainActivity.java
+++ src/com/floern/rhabarber/MainActivity.java
@@ -1,6 +1,5 @@
 package com.floern.rhabarber;
 
-import com.floern.rhabarber.physicstest.PhysicsTestActivity;
 import com.floern.rhabarber.sensorgltest.SensorTestOpenGLActivity;
 
 import android.os.Bundle;
@@ -26,15 +25,6 @@ public class MainActivity extends Activity {
     	startActivity(new Intent(this, SensorTestOpenGLActivity.class));
     }
     
-    /**
-     * onClick event for starting physics test activity
-     * @param v pressed Button
-     */
-    public void onPhysicsTestActivityButtonPressed(View v) {
-    	startActivity(new Intent(this, PhysicsTestActivity.class));
-    }
-    
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
