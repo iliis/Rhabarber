c4144ab08601cd69ee40e8cbe2f035bfc8814320
iliis <samuelbryner@gmx.ch>
2012-12-10 20:15:03 +0100

    Implemented deep copy of ClientStateAccumulator.

--- src/com/floern/rhabarber/network2/ClientStateAccumulator.java
+++ src/com/floern/rhabarber/network2/ClientStateAccumulator.java
@@ -1,15 +1,48 @@
 package com.floern.rhabarber.network2;
 
+import java.util.ArrayList;
+
 import com.floern.rhabarber.network2.GameNetworkingProtocolConnection.Message;
 
 public class ClientStateAccumulator {
 	
-	public void update(Message m) {
+	enum UserInputWalk {
+		LEFT,
+		RIGHT,
+		NONE;
+	}
+	
+	private class Acceleration {
+		public float x,y,z;
 		
+		public Acceleration copy() {
+			Acceleration a = new Acceleration();
+			
+			a.x = this.x;
+			a.y = this.y;
+			a.z = this.y;
+			
+			return a;
+		}
+	}
+	
+	public ArrayList<UserInputWalk> inputs = new ArrayList<UserInputWalk>();
+	public ArrayList<Acceleration>  accels = new ArrayList<Acceleration>();
+	
+	public void update(Message m) {
+		// TODO
 	}
 	
 	// creates a deep copy of this
 	public ClientStateAccumulator copy() {
-		return null;
+		ClientStateAccumulator c = new ClientStateAccumulator();
+		
+		for(UserInputWalk w: inputs)
+			c.inputs.add(w);
+		
+		for(Acceleration a: accels)
+			c.accels.add(a.copy());
+		
+		return c;
 	}
 }
